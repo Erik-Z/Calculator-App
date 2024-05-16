4907a0db4740d64d9cc963fa325856932ee8c1e4
Erik-Z <erikzhou7@gmail.com>
2024-05-15 22:58:06 -0400

    Delete unused state

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.*
 import com.example.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,9 +12,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            val result = remember {
-                mutableStateOf("0")
-            }
             CalculatorTheme {
                 val viewModel = CalculatorViewModel()
                 CalculatorUI(
